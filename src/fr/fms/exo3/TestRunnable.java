ea14cdd279cc7709d763585d0572b8e60c8405bd
matthieu-27 <matthieu27@pm.me>
2025-12-16 13:46:21 +0100

    Exercice 3.3: TestRunnable

--- src/fr/fms/exo3/TestRunnable.java
+++ src/fr/fms/exo3/TestRunnable.java
@@ -3,8 +3,9 @@ package fr.fms.exo3;
 public class TestRunnable implements Runnable {
 	@Override
 	public void run() {
-		for (int i = 0; i < 10; i++) {
-			System.out.println(this.toString());
+		String[] str = {"!*!","\"**\"","#***#","$****$","%*****%","&******&","'*******'","(********(",")**********)","************"};
+		for (int i = 0; i < str.length; i++) {
+			System.out.println(str[i]);
 		}
 	}
 	
